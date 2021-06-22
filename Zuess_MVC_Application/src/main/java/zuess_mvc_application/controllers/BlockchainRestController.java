1863b869a538125414f06c9bfeb55c8f5df96125
Robert Meis <rmeis@safesplash.com>
2021-06-22 14:54:03 -0600

    Adds all outstanding OtterCoin method calls:
    -getName()
    -getSymbol()
    -getStandard()
    -approve()
    -transferFrom()
    -balanceOf()

--- Zuess_MVC_Application/src/main/java/zuess_mvc_application/controllers/BlockchainRestController.java
+++ Zuess_MVC_Application/src/main/java/zuess_mvc_application/controllers/BlockchainRestController.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Future;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import zuess_mvc_application.services.*;
@@ -16,14 +17,29 @@ public class BlockchainRestController {
 	BlockchainService blockchainService;
 	
 	@GetMapping("/api/test")
-	public void callEthereumNetwork() throws InterruptedException, ExecutionException {
+	public void testEthereumNetwork(
+			@RequestParam("adminPrivateKey") String adminPrivateKey
+			) throws InterruptedException, ExecutionException {
 		
 		/***Deploy Smart Contract and test call methods on deployed Smart Contract***/
 		try
 		{
-			blockchainService.deploySmartContract("");
+			String ADMIN_ACCOUNT = "0x738F657A0e9F102528c1160E04554a89747aAa1e";
+			String TEST_ACCOUNT = "0xc37F977738df1bC264D26FF2e906471Bf222Ea63";
+			
+			//Contract info functions
+			blockchainService.deploySmartContract(adminPrivateKey);
+			blockchainService.getContractName();
+			blockchainService.getContractSymbol();
+			blockchainService.getContractStandard();
 			blockchainService.getContractBalance();
-			blockchainService.transferFunds("0xdaEc996523902b545BF564155FCdC1F235C7de6A", 10);
+			
+			//Fund transfer functions
+			blockchainService.transferFunds(TEST_ACCOUNT, 10);
+			blockchainService.getBalance(TEST_ACCOUNT);
+			blockchainService.approveAllowance(ADMIN_ACCOUNT, 100);
+			blockchainService.transferAllowanceFunds(ADMIN_ACCOUNT, TEST_ACCOUNT, 50);
+			blockchainService.getBalance(TEST_ACCOUNT);
 		} catch (Exception e)
 		{
 			// TODO Auto-generated catch block
