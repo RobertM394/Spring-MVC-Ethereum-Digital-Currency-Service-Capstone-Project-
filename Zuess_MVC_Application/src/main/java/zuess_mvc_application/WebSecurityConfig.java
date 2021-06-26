7f0d55c6b6e5455c5d41a8d83e64a370958e7746
JesseKrepelka <jessekrepelka@gmail.com>
2021-06-26 16:11:46 -0700

    Disable security temporarily.

--- Zuess_MVC_Application/src/main/java/zuess_mvc_application/WebSecurityConfig.java
+++ Zuess_MVC_Application/src/main/java/zuess_mvc_application/WebSecurityConfig.java
@@ -48,15 +48,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure (HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//			.antMatchers("/users").authenticated()
+//	        .anyRequest().permitAll()
+//	        .and()
+//	        .formLogin()
+//	            .usernameParameter("email")
+//	            .defaultSuccessUrl("/accountInfo")
+//	            .permitAll()
+//	        .and()
+//	        .logout().logoutSuccessUrl("/").permitAll();
 		http.authorizeRequests()
-			.antMatchers("/users").authenticated()
-	        .anyRequest().permitAll()
+	        .anyRequest().authenticated()
 	        .and()
-	        .formLogin()
-	            .usernameParameter("email")
-	            .defaultSuccessUrl("/accountInfo")
-	            .permitAll()
+	    .formLogin()
 	        .and()
-	        .logout().logoutSuccessUrl("/").permitAll();
+	    .httpBasic().disable();
 	}
 }
