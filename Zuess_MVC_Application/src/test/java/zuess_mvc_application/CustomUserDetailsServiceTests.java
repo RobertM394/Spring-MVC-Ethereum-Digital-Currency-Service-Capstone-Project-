bc6db69487567edc96a0bd9e462b23b8a66fe1f0
Antonio Villagomez <avillagomez@csumb.edu>
2021-07-31 20:57:15 -0700

    CustomeUDService test

---
+++ Zuess_MVC_Application/src/test/java/zuess_mvc_application/CustomUserDetailsServiceTests.java
@@ -0,0 +1,62 @@
+package zuess_mvc_application;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.BDDMockito.given;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+
+import static org.mockito.ArgumentMatchers.anyString;
+import zuess_mvc_application.domain.User;
+import zuess_mvc_application.repository.UserRepository;
+import zuess_mvc_application.services.BlockchainService;
+import zuess_mvc_application.services.CustomUserDetailsService;
+
+@SpringBootTest
+public class CustomUserDetailsServiceTests {
+
+	@MockBean
+	UserRepository userRepository;
+
+	//Class being tested
+	@Autowired
+	CustomUserDetailsService customUDService;
+	
+	@MockBean
+	BlockchainService blockchainService;
+	
+	@BeforeEach
+	public void setUpEach() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void shouldRetrieveUserByEmail() {
+		
+		User expected = new User("test@email.com","pw","test","lname","testAcctId",50.00,1);
+		
+		
+		given(userRepository.findByEmail("test@email.com")).willReturn(expected);
+		
+		User result = customUDService.retrieveUserByEmail("test@email.com");	
+		
+		assertEquals(result, expected);
+	
+	}
+	@Test
+	public void userNull() {
+
+		given(userRepository.findByEmail(anyString())).willReturn(null);
+		
+		User actual = customUDService.retrieveUserByEmail("test@email.com");
+		
+		assertNull(actual);
+	}
+	
+
+}
